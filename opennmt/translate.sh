178e4d62fca39c365fa9a708601236558dc99fb1
Artem Oboturov <oboturov@gmail.com>
2018-02-02 18:02:40 +0100

    Working OpenNMT supervised model.

--- opennmt/translate.sh
+++ opennmt/translate.sh
@@ -7,21 +7,32 @@ src=src
 tgt=tgt
 epochs=$1   # 13
 
+cut -f 1 /data/parallel_corpus.txt > /model/parallel.$src
+cut -f 2 /data/parallel_corpus.txt > /model/parallel.$tgt
+
 # Train-Validation split
-lines=`wc -l < /data/parallel.$src`
+lines=`wc -l < /model/parallel.$src`
 lines_train=`python3 -c "print(int($lines*.90))"`
 lines_val=`python3 -c "print($lines-$lines_train)"`
 
 # Tokenize data
-head -n$lines_train /data/parallel.$src | th $opennmt/tools/tokenize.lua > /model/train.$src.txt
-tail -n$lines_val   /data/parallel.$src | th $opennmt/tools/tokenize.lua > /model/valid.$src.txt
-head -n$lines_train /data/parallel.$tgt | th $opennmt/tools/tokenize.lua > /model/train.$tgt.txt
-tail -n$lines_val   /data/parallel.$tgt | th $opennmt/tools/tokenize.lua > /model/valid.$tgt.txt
+head -n$lines_train /model/parallel.$src | th $opennmt/tools/tokenize.lua > /model/train.$src.txt
+tail -n$lines_val   /model/parallel.$src | th $opennmt/tools/tokenize.lua > /model/valid.$src.txt
+head -n$lines_train /model/parallel.$tgt | th $opennmt/tools/tokenize.lua > /model/train.$tgt.txt
+tail -n$lines_val   /model/parallel.$tgt | th $opennmt/tools/tokenize.lua > /model/valid.$tgt.txt
 
 # PreProcess
 th $opennmt/preprocess.lua -train_src /model/train.$src.txt -train_tgt /model/train.$tgt.txt -valid_src /model/valid.$src.txt -valid_tgt /model/valid.$tgt.txt -save_data /model/pretrained
 
-# OpenNMT
-th $opennmt/train.lua -data data/pretrained.t7 -save_model /model/data/pretrained -gpuid 1
+# Train
+th $opennmt/train.lua -optim adam -learning_rate 0.0002 -end_epoch $epochs -data /model/pretrained-train.t7 -save_model /model/prepared
+
+# Translate
+cat /data/input.txt | th $opennmt/tools/tokenize.lua > /model/input.txt
+
+th $opennmt/translate.lua -model /model/prepared_epoch${epochs}_*.t7 -src /model/input.txt -output /model/output.txt
+
+cat /model/output.txt | th $opennmt/tools/detokenize.lua > /output/output.txt
 
 echo "DONE"
+
