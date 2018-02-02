fe888c08cdd5c659e9ecd4f6e289c24469a9c445
Artem Oboturov <oboturov@gmail.com>
2018-02-02 21:22:57 +0100

    hyperparamopt-v2

--- lang-detect-v3/translate.sh
+++ lang-detect-v3/translate.sh
@@ -8,7 +8,7 @@ if [ $lines -lt 101 ]; then
   exit 0
 fi
 
-langs=("af" "ar" "bg" "bn" "ca" "cs" "cy" "da" "de" "el")
+langs=("et" "fa" "fi" "gu" "he" "hi" "hr" "hu" "id" "it")
 
 l=`python /model/lang_detect.py --corpus /data/corpus2.txt`
 (for e in "${langs[@]}"; do [[ "$e" == "$l" ]] && exit 0; done) && { #found
